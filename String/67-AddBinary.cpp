19e092dd3a50bacfde7b6a59fc6b23e72aaa9c9e
Xiaoyu Bai <xiaoyubai@Xiaoyus-MacBook-Pro.local>
2017-01-12 15:24:59 -0600

    more

---
+++ String/67-AddBinary.cpp
@@ -0,0 +1,44 @@
+/**************************************************************************
+ * File Name : 67-AddBinary.cpp
+ *
+ * Purpose :
+ *
+ * Creation Date : 12-01-2017
+ *
+ * Last Modified : Thu Jan 12 15:24:44 2017
+ *
+ * Created By :  Renne Bai
+**************************************************************************/
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        
+        string result;
+        int carry = 0;
+        int digit;
+        string::reverse_iterator rit_a, rit_b;
+        for(rit_a = a.rbegin(), rit_b = b.rbegin(); rit_a!= a.rend() && rit_b!=b.rend(); rit_a++, rit_b++){
+            digit = carry + (*rit_a - '0') + (*rit_b-'0');
+            result.insert(result.begin(), (digit%2 +'0'));
+            carry = digit/2;
+        }
+        
+        while(rit_a!=a.rend()){
+            digit = carry + (*rit_a - '0');
+            result.insert(result.begin(), (digit%2 +'0'));
+            carry = digit/2;
+            rit_a++;
+        }
+        
+        while(rit_b!=b.rend()){
+            digit = carry + (*rit_b - '0');
+            result.insert(result.begin(), (digit%2 +'0'));
+            carry = digit/2;
+            rit_b++;
+        }
+        
+        if(carry) result.insert(result.begin(),'1');
+        
+        return result;
+    }
+};
