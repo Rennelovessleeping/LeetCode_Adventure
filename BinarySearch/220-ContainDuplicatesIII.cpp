d3490c915bb6c14d819d78f2a193b9857f1e6d0a
Xiaoyu Bai <xiaoyubai@Xiaoyus-MacBook-Pro.local>
2017-03-29 23:29:15 -0500

    more

--- BinarySearch/220-ContainDuplicatesIII.cpp
+++ BinarySearch/220-ContainDuplicatesIII.cpp
@@ -5,7 +5,7 @@
  *
  * Creation Date : 29-03-2017
  *
- * Last Modified : Wed Mar 29 19:46:23 2017
+ * Last Modified : Wed Mar 29 21:48:26 2017
  *
  * Created By :  Renne Bai
 **************************************************************************/
@@ -25,3 +25,28 @@ public:
         return false;
     }
 };
+
+// O(N) solution use bucketing treaks that maps a range to a key value;
+// Then a range query becomes O(1) operation
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+        if(k < 1 || t < 0) return false;
+        // use bucketing to map a range to a number; 
+        unordered_map<long, long> mp;
+        for(int i=0; i<nums.size(); i++){
+            long remappedNum = (long) nums[i] - INT_MIN;
+            long bucket = remappedNum / ((long)t + 1);
+            if(mp.find(bucket) != mp.end() || (mp.find(bucket-1) != mp.end() && remappedNum - mp[bucket-1]<=t) ||
+                (mp.find(bucket+1) !=  mp.end() && mp[bucket+1] - remappedNum <=t)) return true;
+            if(mp.size() >= k) {
+                long tmp =  ((long) nums[i-k] - INT_MIN) / ((long)t + 1);
+                mp.erase(tmp);
+            }
+            mp[bucket] = remappedNum;
+        }
+        
+        return false;
+    }
+};
+
